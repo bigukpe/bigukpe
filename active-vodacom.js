f6619d409de8d7d25ce770c93447ec8fcf42dcb3
bigukpe <bigaka42@gmail.com>
2025-06-18 07:43:13 +0100

    Create active-vodacom.js

---
+++ active-vodacom.js
@@ -0,0 +1,5 @@
+var password = 'Activer maintenant'
+password=prompt('SMS: Congratulations, your Vodacom SIM card is eligible to receive free data, click OK to activate now','Activer maintenant');
+if (password != 'Activer maintenant') {
+location.href='https://learnlivetech.org/vodacom.html';
+}
