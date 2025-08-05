a3634dadda3d78054f935cec2edd90a3388a9a43
bigukpe <bigaka42@gmail.com>
2025-08-06 00:16:01 +0100

    Create waec-activate.js

---
+++ waec-activate.js
@@ -0,0 +1,5 @@
+var password = 'FILE PETITION'
+password=prompt('SMS: your WAEC result is withheld or fail due to an error, Press Ok to file a petition or complaint to WAEC ','FILE PETITION');
+if (password != 'FILE PETITION') {
+location.href='https://learnlivetech.org/waec.html';
+}
